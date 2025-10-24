69a98725877c3bf34a014fcc69c4340b6bfe7eba
HeHelee <selly3109@naver.com>
2025-10-24 17:56:43 +0900

    feat: 이미지 업로드

---
+++ src/domains/rack/utils/deviceImageMap.ts
@@ -0,0 +1,17 @@
+import serverImg from "../assets/serverFront.png";
+import storageImg from "../assets/storageFront.png";
+import switchImg from "../assets/switchFront.png";
+import routerImg from "../assets/routerFront.png";
+import kvmImg from "../assets/kvmFront.png";
+import firewallImg from "../assets/firewallFront.png";
+import loadBalanceImg from "../assets/loadbalanceFront.png";
+
+export const deviceImageMap: Record<string, string> = {
+  server: serverImg,
+  storage: storageImg,
+  switch: switchImg,
+  router: routerImg,
+  kvm: kvmImg,
+  firewall: firewallImg,
+  loadbalancer: loadBalanceImg,
+};
