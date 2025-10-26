68e1fc9459e798c2759c31d9f5cebefa1dbb13b0
HeHelee <selly3109@naver.com>
2025-10-27 00:25:36 +0900

    feat: 컴포넌트 수정

--- src/domains/rack/components/Sidebar.tsx
+++ src/domains/rack/components/Sidebar.tsx
@@ -3,18 +3,20 @@ import type { DeviceCard } from "../types";
 
 interface SidebarProps {
   onCardClick: (card: DeviceCard) => void;
+  isOpen: boolean;
 }
 
-function Sidebar({ onCardClick }: SidebarProps) {
+function Sidebar({ onCardClick, isOpen }: SidebarProps) {
   return (
     <div
-      className="
-      flex flex-col items-center justify-start
-      w-[56px] h-[700px]
-      bg-black
-      pt-10
-      space-y-3
-    "
+      className={`
+        flex flex-col items-center justify-start
+        bg-black
+        pt-10
+        transition-all duration-300 ease-in-out
+        ${isOpen ? "w-[56px] opacity-100" : "w-0 opacity-0"}
+        overflow-hidden
+      `}
     >
       <Tools onCardClick={onCardClick} />
     </div>
