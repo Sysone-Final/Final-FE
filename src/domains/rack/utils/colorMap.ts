0007f446913c44cc250fc79a04a03cea79a2fbb8
HeHelee <selly3109@naver.com>
2025-10-30 15:57:59 +0900

    feat: 타입 변경

--- src/domains/rack/utils/colorMap.ts
+++ src/domains/rack/utils/colorMap.ts
@@ -1,11 +1,9 @@
 export const typeColorMap: Record<string, string> = {
-  server: "#38bdf8",
-  switch: "#E80054",
-  router: "#fbbf24",
-  storage: "#34d399",
-  firewall: "#8B0000",
-  loadbalancer: "#06b6d4",
-  pdu: "#f59e0b",
-  kvm: "#ec4899",
-  other: "#64748b",
+  SERVER: "#38bdf8",
+  SWITCH: "#E80054",
+  ROUTER: "#fbbf24",
+  STORAGE: "#34d399",
+  FIREWALL: "#8B0000",
+  LOAD_BALANCER: "#06b6d4",
+  KVM: "#ec4899",
 };
