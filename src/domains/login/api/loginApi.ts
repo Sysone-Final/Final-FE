9c2054ace6a1b90a3ffa58868f518db9a5d87abf
HeHelee <selly3109@naver.com>
2025-11-05 15:23:46 +0900

    feat: login API 반환값 수정

--- src/domains/login/api/loginApi.ts
+++ src/domains/login/api/loginApi.ts
@@ -4,5 +4,5 @@ import type { LoginRequest, LoginResponse } from "../types/login";
 export const loginApi = async (data: LoginRequest): Promise<LoginResponse> => {
   console.log("Using client:", client.defaults.withCredentials);
   const response = await client.post("/auth/login", data);
-  return response.data;
+  return response.data.result;
 };
