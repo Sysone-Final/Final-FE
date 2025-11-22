73edaf5102f711ce67d33c4e7856c5024214940c
HeHelee <selly3109@naver.com>
2025-11-22 23:05:44 +0900

    feat: 응답 타입 정의

--- src/domains/serverView/serverDashboard/types/index.ts
+++ src/domains/serverView/serverDashboard/types/index.ts
@@ -14,3 +14,73 @@ export interface UpdateEquipmentRequest
   diskThresholdWarning: number;
   diskThresholdCritical: number;
 }
+
+export interface SystemMonitoringData {
+  id: number;
+  equipmentId: number;
+  generateTime: string;
+  cpuIdle: number;
+  cpuUser: number;
+  cpuSystem: number;
+  cpuWait: number;
+  cpuNice: number;
+  cpuIrq: number;
+  cpuSoftirq: number;
+  cpuSteal: number;
+  loadAvg1: number;
+  loadAvg5: number;
+  loadAvg15: number;
+  contextSwitches: number;
+  totalMemory: number;
+  usedMemory: number;
+  freeMemory: number;
+  usedMemoryPercentage: number;
+  memoryBuffers: number;
+  memoryCached: number;
+  memoryActive: number;
+  memoryInactive: number;
+  totalSwap: number;
+  usedSwap: number;
+  usedSwapPercentage: number;
+}
+
+export interface DiskMonitoringData {
+  id: number;
+  equipmentId: number;
+  generateTime: string;
+  totalBytes: number;
+  usedBytes: number;
+  freeBytes: number;
+  usedPercentage: number;
+  ioReadBps: number;
+  ioWriteBps: number;
+  ioTimePercentage: number | null;
+  ioReadCount: number;
+  ioWriteCount: number;
+  totalInodes: number;
+  usedInodes: number;
+  freeInodes: number;
+  usedInodePercentage: number;
+}
+
+export interface NetworkMonitoringData {
+  id: number;
+  equipmentId: number;
+  nicName: string;
+  generateTime: string;
+  rxUsage: number | null;
+  txUsage: number | null;
+  inPktsTot: number;
+  outPktsTot: number;
+  inBytesTot: number;
+  outBytesTot: number;
+  inBytesPerSec: number;
+  outBytesPerSec: number;
+  inPktsPerSec: number;
+  outPktsPerSec: number;
+  inErrorPktsTot: number;
+  outErrorPktsTot: number;
+  inDiscardPktsTot: number;
+  outDiscardPktsTot: number;
+  operStatus: number;
+}
